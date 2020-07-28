b4140c78229ce4b78d0cd145eb05d337fea9bc22
wendelspires <wendelspires@hotmail.com>
2020-07-28 16:17:13 -0500

    First Commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// we dont use var anymore, only let or const. Const can't be changed
+// to use constant you have always to add de value when you write const
+
+const name = 'Jhon';
+const age = 30;
+//const rating = 4.5;
+//const isCool = true;
+//const x = null;
+//const y = undefined;
+//let z;
+
+//console.log(typeof y);
+
+// Concatenation
+//console.log('My name is ' + name + ' and I am ' + age);
+
+//template strings
+
+const hello = (`My name is ${name} and I am ${age}`);
+console.log(hello);
