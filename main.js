3692276a5a040783b8f9700e0a78bb860ae965c7
wendelspires <wendelspires@hotmail.com>
2020-07-28 16:31:03 -0500

    Edit Strings

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // we dont use var anymore, only let or const. Const can't be changed
 // to use constant you have always to add de value when you write const
 
-const name = 'Jhon';
-const age = 30;
+//const name = 'Jhon';
+//const age = 30;
 //const rating = 4.5;
 //const isCool = true;
 //const x = null;
@@ -16,5 +16,19 @@ const age = 30;
 
 //template strings
 
-const hello = (`My name is ${name} and I am ${age}`);
-console.log(hello);
+//const hello = (`My name is ${name} and I am ${age}`);
+//console.log(hello);
+
+const s = 'Technology, computer, it, code';
+
+//length of constant
+//console.log(s.length);
+
+// edit strings
+
+//console.log(s.toUpperCase());
+//console.log(s.toLowerCase());
+//substring = s.substring(0,5);
+//console.log (substring.toUpperCase());
+//divide
+console.log(s.split(', '));
