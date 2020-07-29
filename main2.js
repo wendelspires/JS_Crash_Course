b5b2988a13f3a4ef95cef2f4829b413e271917c7
wendelspires <wendelspires@hotmail.com>
2020-07-29 13:31:48 -0500

    The main 2

---
+++ main2.js
@@ -0,0 +1,14 @@
+const person = {
+  firstName: 'Wendel',
+  lastName: 'Pires',
+  age: 30,
+  hobbies: ['music','movies', 'sports'],
+  addres: {
+    street: 'rua mario cypreste',
+    city: 'Vitoria',
+    state: 'ES'
+  }
+}
+
+//console.log(person.firstName,person.lastName);
+console.log(person.hobbies['1']);
